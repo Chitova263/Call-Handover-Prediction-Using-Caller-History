1186a9ffdb7c2726e05001984f63f824d83f707d
Chitova263 <mknnig001@myuct.ac.za>
2019-09-16 18:13:50 +0200

    deleted CallAdmissionControl folder, improved predictive algorithm

--- Network/Rat.cs
+++ Network/Rat.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Collections.Generic;
-using VerticalHandoverPrediction.CallAdmissionControl;
+using VerticalHandoverPrediction.Cac;
 using VerticalHandoverPrediction.CallSession;
 using VerticalHandoverPrediction.Mobile;
 
--- Program.cs
+++ Program.cs
@@ -17,23 +17,28 @@ namespace VerticalHandoverPrediction
   
             HetNet._HetNet.GenerateRats();
 
-            HetNet._HetNet.GenerateUsers(10);
+            HetNet._HetNet.GenerateUsers(20);
             
             Utils.CsvUtils._Instance.Clear($"{Environment.CurrentDirectory}/start.csv");
             Utils.CsvUtils._Instance.Clear($"{Environment.CurrentDirectory}/end.csv");
             
             for (int i = 0; i < 10; i++)
             {
-                NetworkSimulator._NetworkSimulator.Run(200, true, false);
+                NetworkSimulator._NetworkSimulator.Run(500, true, false);
             }
+
             NetworkSimulator._NetworkSimulator.UseCallLogs = false;
             
             NetworkSimulator._NetworkSimulator.Run(10, false, false);
-            HetNet._HetNet.Dump();
-            NetworkSimulator._NetworkSimulator.UseCallLogs = false;
 
+            System.Console.WriteLine(HetNet._HetNet.CallsGenerated);
+            System.Console.WriteLine("NonPredictive      Predictive");
+            System.Console.Write(HetNet._HetNet.VerticalHandovers + "         ");
+            
             NetworkSimulator._NetworkSimulator.Run(10, false, true);
-            HetNet._HetNet.Dump();
+            System.Console.Write(HetNet._HetNet.VerticalHandovers);
+
+            System.Console.WriteLine();
         }
     }
 }
