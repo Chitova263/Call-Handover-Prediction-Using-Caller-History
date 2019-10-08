e75bfe71d5cc87a5d945ab18c10074d8ef741e74
Chitova263 <mknnig001@myuct.ac.za>
2019-10-08 20:30:46 +0200

    corrected call history data

--- src/Core/Program.cs
+++ src/Core/Program.cs
@@ -62,9 +62,9 @@
                 
                 foreach (var log in HetNet.Instance.MobileTerminals.SelectMany(x => x.CallLogs))
                 {
-                    // Utils.CsvUtils._Instance.Write<CallLogMap, CallLog>(
-                    //     log, 
-                    //     $"{Environment.CurrentDirectory}/calllogs.csv");
+                    Utils.CsvUtils._Instance.Write<CallLogMap, CallLog>(
+                         log, 
+                         $"{Environment.CurrentDirectory}/calllogs.csv");
                     //load history in memory    
                     HetNet.Instance.CallerHistory.Add(log);
                 }
@@ -146,8 +146,8 @@
                     NetworkSimulator.Instance.Events.Clear();            
                 } 
 
-                Utils.CsvUtils._Instance.Clear($"{Environment.CurrentDirectory}/SimResults.csv"); 
-                Utils.CsvUtils._Instance.Clear($"{Environment.CurrentDirectory}/calllogs.csv"); 
+                //Utils.CsvUtils._Instance.Clear($"{Environment.CurrentDirectory}/SimResults.csv"); 
+                //Utils.CsvUtils._Instance.Clear($"{Environment.CurrentDirectory}/calllogs.csv"); 
                 
                 return results;
             });
